46267cccc3ea042f11178dd0ba0d0f9dbeb739be
Your Name <you@example.com>
2025-01-29 11:24:48 +0330

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,31 @@
+import Inspect from "vite-plugin-inspect";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    
+    Inspect(),
+    visualizer({ open: true }),
+  ],
   resolve: {
     alias: {
       "/images": "/src/assets/images",
     },
     extensions: [".js", ".jsx"],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          swiper: ['swiper'],
+          'framer-motion': ['framer-motion'],
+        },
+      },
+    },
+    minify: 'esbuild',
   },
 });
